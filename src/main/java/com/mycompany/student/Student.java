b17dec6077b4f61f4ea060adf79200583b0e530a
Ian Cewa <cewaian@gmail.com>
2026-08-18 10:43:39 +0300

    The first file with the GUI

---
+++ src/main/java/com/mycompany/student/Student.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.student;
+
+/**
+ *
+ * @author Administrator
+ */
+import java.awt.*;
+import javax.swing.*;
+import java.util.*;
+public class Student {
+     static ArrayList<StudentManagement> students=new ArrayList<>();
+    public static void main(String[] args) {
+        JFrame frame=new JFrame("Student Grade Calculator");
+       
+        JPanel panel=new JPanel();
+        panel.setLayout(new GridLayout(4,2));
+        JLabel nameLabel=new JLabel("Name: ");
+        JTextField nameTxt=new JTextField(10);
+        JLabel courseLabel=new JLabel("Course: ");
+        JComboBox<String> courseBox=new JComboBox<>(new String[]{"Computer Science","IT","Computer Technology","Software Engineering"}); 
+        JLabel gpaLabel=new JLabel("GPA: ");
+        JTextField gpaTxt=new JTextField(10);
+       JLabel ageLabel=new JLabel("Age: ");
+       JTextField ageTxt=new JTextField(5);
+ 
+        JPanel p2=new JPanel();
+        JButton resultButton=new JButton("Result");
+        resultButton.addActionListener(e->{
+           try{
+                String name=nameTxt.getText().trim().replaceAll("\\s+"," ");
+                  if(name.isEmpty()){
+               JOptionPane.showMessageDialog(frame,"Name cant be empty");
+               return;}  
+                  if(!name.matches("[a-zA-Z]+")){
+                      JOptionPane.showMessageDialog(frame,"Name must contain letters only");
+                      return;
+                  }
+           double gpa=Double.parseDouble(gpaTxt.getText().trim());
+           if(gpa<0||gpa>4){
+               JOptionPane.showMessageDialog(frame,"GPA enmtered should be btn 0 and 4");
+               return;
+           }
+           String course=(String)courseBox.getSelectedItem();           
+           int age=Integer.parseInt(ageTxt.getText().trim());
+           if(age<18){
+               JOptionPane.showMessageDialog(frame,"Invalid Age");
+               return;
+           }
+            StudentManagement s=new StudentManagement(name,age,course,gpa);
+            students.add(s);
+           JOptionPane.showMessageDialog(frame, "Name :"+name+"\nCourse: "+course+"\nGPA: "+gpa);
+           }
+           
+           catch(NumberFormatException m){
+               JOptionPane.showMessageDialog(frame,"Invalid age or gpa entered");
+            }
+           
+        });
+        
+        panel.add(nameLabel);
+        panel.add(nameTxt);
+        panel.add(courseLabel);
+        panel.add(courseBox);
+        panel.add(gpaLabel);
+        panel.add(gpaTxt);
+        panel.add(ageLabel);
+        panel.add(ageTxt);
+        p2.add(resultButton);
+        frame.add(panel,BorderLayout.CENTER);
+        frame.add(p2,BorderLayout.SOUTH);
+        panel.setBackground(Color.gray);
+        p2.setBackground(Color.pink);
+        frame.setSize(500,300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        
+    }}
---
+++ src/main/java/com/mycompany/student/StudentManagement.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.student;
+
+/**
+ *
+ * @author Administrator
+ */
+import javax.swing.*;
+import java.awt.*;
+public class StudentManagement {
+    String name;
+    int age;
+    String course;
+    double gpa;
+    StudentManagement(String name,int age,String course,double gpa){
+        this.name=name;
+        this.age=age;
+        this.course=course;
+        this.gpa=gpa;
+    }
+}
