842bf8faa0adb5aaf90bf5a720ffe28d0c76358a
Ian Cewa <cewaian@gmail.com>
2026-08-21 21:45:44 +0300

    added an update button and fine tuned it

--- src/main/java/com/mycompany/student/Student.java
+++ src/main/java/com/mycompany/student/Student.java
@@ -11,7 +11,7 @@ package com.mycompany.student;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-
+import java.awt.event.*;
 import java.util.*;
 public class Student {
      static DefaultTableModel model=new DefaultTableModel(new String[]{"Name","Age","Course","GPA"
@@ -32,8 +32,22 @@ public class Student {
         JTextField gpaTxt=new JTextField(10);
        JLabel ageLabel=new JLabel("Age: ");
        JTextField ageTxt=new JTextField(5);
- 
+        table.addMouseListener(new MouseAdapter(){
+        public void mouseClicked(MouseEvent l){
+            int row=table.getSelectedRow();
+            if(row!=-1){
+                StudentManagement s=students.get(row);
+                nameTxt.setText(s.name);
+                ageTxt.setText(String.valueOf(s.age));
+                courseBox.setSelectedItem(s.course);
+                gpaTxt.setText(String.valueOf(s.gpa));
+            }
+        }
+    });
+        
+       JButton deleteButton=new JButton("DELETE");
         JPanel p2=new JPanel();
+        JButton updateButton=new JButton("UPDATE");
         JButton resultButton=new JButton("Result");
         resultButton.addActionListener(e->{
            try{
@@ -41,7 +55,7 @@ public class Student {
                   if(name.isEmpty()){
                JOptionPane.showMessageDialog(frame,"Name cant be empty");
                return;}  
-                  if(!name.matches("[a-zA-Z]+")){
+                  if(!name.matches("[a-zA-Z ]+")){
                       JOptionPane.showMessageDialog(frame,"Name must contain letters only");
                       return;
                   }
@@ -71,7 +85,58 @@ public class Student {
             }
            
         });
-        
+        deleteButton.addActionListener(el->{
+           
+               int row=table.getSelectedRow();
+               if(row==-1){
+                   JOptionPane.showMessageDialog(frame,"Select a student First");
+                   return;
+               }
+               students.remove(row);
+               model.removeRow(row);
+             
+        });
+        updateButton.addActionListener(ej->{
+
+            int row=table.getSelectedRow();
+            if(row==-1){
+                JOptionPane.showMessageDialog(frame,"Select a student first");
+                return;
+            }
+            StudentManagement s=students.get(row);
+            String newName=nameTxt.getText().trim().replaceAll("\\s+"," ");
+            if(newName.isEmpty()||!newName.matches("[a-zA-Z ]+")){
+                JOptionPane.showMessageDialog(frame,"Invalid NAME")
+                        ;return;
+        }
+            
+            int newAge=Integer.parseInt(ageTxt.getText().trim().replaceAll("\\s+",""));
+            if(newAge<18){
+                   JOptionPane.showMessageDialog(frame,"Invalid AGE");
+                   return;
+            }
+            String newCourse=(String)courseBox.getSelectedItem();
+            
+            double newgpa=Double.parseDouble(gpaTxt.getText().trim().replaceAll("\\s+",""));
+            if(newgpa<0||newgpa>4){
+                JOptionPane.showMessageDialog(frame,"Invalid GPA");
+                return;
+            }
+            s.name=newName;
+            s.age=newAge;
+            s.course=newCourse;
+            s.gpa=newgpa;
+            model.setValueAt(newName, row, 0);
+            model.setValueAt(newAge, row, 1);
+            model.setValueAt(newCourse, row, 2);
+            model.setValueAt(newgpa, row, 3);
+            nameTxt.setText("");
+             ageTxt.setText("");
+              courseBox.setSelectedIndex(0);
+               gpaTxt.setText("");
+            
+        });
+  
         panel.add(nameLabel);
         panel.add(nameTxt);
         panel.add(courseLabel);
@@ -81,7 +146,10 @@ public class Student {
         panel.add(ageLabel);
         panel.add(ageTxt);
         tablePanel.add(scrollpane);
+        
         p2.add(resultButton);
+        p2.add(deleteButton);
+        p2.add(updateButton);
         frame.add(panel,BorderLayout.NORTH);
         frame.add(tablePanel,BorderLayout.CENTER);
         frame.add(p2,BorderLayout.SOUTH);
